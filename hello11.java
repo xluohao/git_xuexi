e25a088cf32ac3ef6c75c4570f564965c80e157c
luohao <584725094@qq.com>
2019-07-11 10:16:39 +0800

    添加两个hello文件

---
+++ hello11.java
@@ -0,0 +1,38 @@
+ class Zi {
+	 
+	 
+	 public static	void main (String[] args){
+		 
+	System.out.println("????????");
+	B b=new B();
+//System.out.println(b);
+//System.out.println(B.a1);
+		 
+	 }
+	 
+	public	static  void  method (int a ){
+			
+			 System.out.println("method");
+		}
+		
+		
+		
+ }
+ 
+ 
+ 
+ 
+ 
+
+	
+	    class B {
+		
+			//B a1= new B();
+			// B a1;
+		
+		
+	
+		
+	
+		
+	}
